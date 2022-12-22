4b7dcadc6f7c91e903a2bbf57d289bd9365b7875
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-22 22:17:26 +0100

    creating basic sql tables

---
+++ sql/architecture.sql
@@ -0,0 +1,39 @@
+USE gamesorting_webapp;
+
+-- This table hold the list of all the lists (game list, books list, etc)
+DROP TABLE IF EXISTS lists;
+
+CREATE TABLE IF NOT EXISTS lists(
+    ListID BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(300) NOT NULL -- The name of the list
+);
+
+-- This table hold the user defined rows in their list
+DROP TABLE IF EXISTS listRowsType;
+
+CREATE TABLE IF NOT EXISTS listRowsType(
+    ListRowsTypeID BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(300) NOT NULL, -- The name of the custom row
+    Type VARCHAR(300) NOT NULL, -- The type of the custom row
+    Position TINYINT UNSIGNED NOT NULL -- The position of the custom row
+);
+
+-- This table hold the items of each lists
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE IF NOT EXISTS items(
+    ItemsID BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ListID BIGINT UNSIGNED NOT NULL, -- To wich list the item is own
+    Name VARCHAR(300) NOT NULL, -- The name of the item
+    URL VARCHAR(10000)
+);
+
+-- This table hold the values of each custom rows of each items
+DROP TABLE IF EXISTS customRowsItems;
+
+CREATE TABLE IF NOT EXISTS customRowsItems(
+    CustomRowsItemsID BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ItemsID BIGINT UNSIGNED NOT NULL, -- To wich list item this custom items is linked to
+    ListRowsTypeID BIGINT UNSIGNED NOT NULL, -- To wich list custom rows this custom items in linked to.
+    Name VARCHAR(300) NOT NULL -- The name of the custom item
+);
