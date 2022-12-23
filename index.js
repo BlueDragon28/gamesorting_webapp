f28ab5900a84e1ce0f8639cea9ed459dd144d8a0
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-23 16:28:36 +0100

    adding ejs engine

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /*
-Required packages and js files.
+Required packages and js files
 */
+const path = require("path");
 const express = require("express");
 
-// Initializeing express.
+// Initializeing express
 const app = express();
+
+// Use EJS has a view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.send("<h1>GameSorting index!</h1>");
