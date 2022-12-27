1c971f43c117947cf4e85686a88ac931c30a109f
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-27 15:39:26 +0100

    sql: retrieve lists available in a collection

--- models/gameSortingDB.js
+++ models/gameSortingDB.js
@@ -3,15 +3,17 @@ All the SQL interaction with the gamesorting_webapp SQL databases are made here.
 */
 const mariadb = require("../sql/connection");
 const collections = require("./collections");
+const lists = require("./lists");
 
 const Tables = {
-    COLLECTIONS: "collections"
+    COLLECTIONS: "collections",
+    LISTS: "lists"
 };
 
 /*
 Retrieving all the data of a specific table.
 */
-async function retrieveAllData(connection, table) {
+async function retrieveAllData(connection, table, args) {
     if (!connection) {
         return null;
     }
@@ -21,6 +23,8 @@ async function retrieveAllData(connection, table) {
     case Tables.COLLECTIONS:
         return await collections.find(connection);
 
+    case Tables.LISTS:
+        return await lists.find(connection, ...args);
     }
 }
 
@@ -31,7 +35,7 @@ module.exports = {
     /*
     Retrieve data from a specific table
     */
-    find: async (table) => {
+    find: async (table, ...args) => {
         if (!table || typeof table !== "string" || table.length === 0) {
             return null;
         }
@@ -41,7 +45,7 @@ module.exports = {
             return null;
         }
 
-        const queryData = await retrieveAllData(connection, table);
+        const queryData = await retrieveAllData(connection, table, args);
         connection.close();
         return queryData;
     }
---
+++ models/lists.js
@@ -0,0 +1,47 @@
+/*
+Statement to query the lists available in a collection.
+*/
+function strRetrieveListsFromCollection(collectionID) {
+    if (!collectionID ||
+            (typeof collectionID !== "number" &&
+             typeof collectionID !== "bigint" &&
+             typeof collectionID !== "string")) {
+        return null;
+    }
+
+    if (typeof collectionID === "string") {
+        try {
+            collectionID = BigInt(collectionID);
+        } catch {
+            return null;
+        }
+    }
+
+    return `SELECT ListID, Name FROM lists WHERE CollectionID = ${collectionID.toString()}`;
+}
+
+module.exports = {
+    /*
+    Returning the lists available inside a collection.
+    */
+    find: async (connection, collectionID) => {
+        if (!connection) {
+            return null;
+        }
+
+        const strStatement = strRetrieveListsFromCollection(collectionID);
+
+        if (!strStatement) {
+            return null;
+        }
+
+        let queryResult = null;
+        try {
+            queryResult = connection.query(strStatement);
+        } catch (error) {
+            console.error(`Failed to query data from lists SQL table\n\t${error}`)
+        }
+
+        return queryResult;
+    }
+}
