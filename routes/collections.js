954839357c2c7c3542547dab4d1695c354eaaacf
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-27 15:39:45 +0100

    add route to display lists in a collection

--- routes/collections.js
+++ routes/collections.js
@@ -16,4 +16,19 @@ module.exports = (app) => {
 
         res.render("collections/collectionsIndex.ejs", { collections });
     });
+
+    /*
+    Entry to see the lists available inside a collection
+    */
+    app.get("/collections/:collectionID", async (req, res) => {
+        const { collectionID } = req.params;
+
+        const lists = await database.find(database.LISTS, collectionID);
+
+        if (!lists) {
+            res.send("<h1>Failed to query the lists from the collection");
+        }
+
+        res.render("collections/lists.ejs", { lists });
+    });
 };
