a54627992c381df63d18d5d62dc3bf9e44b7d6bf
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-29 00:04:38 +0100

    retrieve list name and id

--- models/lists.js
+++ models/lists.js
@@ -13,8 +13,14 @@ function strRetrieveListsFromCollection(collectionID) {
     return `SELECT ListID, Name FROM lists WHERE CollectionID = ${collectionID.toString()}`;
 }
 
+function strRetrieveNameAndIDFromListID(listID) {
+    listID = bigint.toBigInt(listID);
+
+    if (!bigint.isValid(listID)) {
             return null;
         }
+
+    return `SELECT ListID, Name FROM lists WHERE ListID = ${listID.toString()}`;
     }
 
     return `SELECT ListID, Name FROM lists WHERE CollectionID = ${collectionID.toString()}`;
@@ -32,6 +38,26 @@ module.exports = {
         }
 
         let queryResult = null;
+        return queryResult;
+    },
+
+    /*
+    Return the name and id of a list from a ListID
+    */
+    findNameAndID: async (connection, listID) => {
+        const strStatement = strRetrieveNameAndIDFromListID(listID);
+
+        if (!connection || !strStatement) {
+            return null;
+        }
+
+        let queryResult = null;
+        try {
+            queryResult = await connection.query(strStatement);
+        } catch (error) {
+            console.error(`Failed to query ListID and Name from list table ${listID}\n\t${error}`);
+        }
+
         try {
             queryResult = await connection.query(strStatement);
         } catch (error) {
