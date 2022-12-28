9280139e16f25c2fecf8d471e5a13235fac3777d
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-28 21:11:03 +0100

    add method override to allow over http verbs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Required packages and js files
 const path = require("path");
 const express = require("express");
 const collectionsRoute = require("./routes/collections");
+const methodOverride = require("method-override");
 
 // Initializeing express
 const app = express();
@@ -13,6 +14,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); // parse body
+app.use(methodOverride("_method")); // Allow the use of http verb not supported by web browsers
 
 app.get("/", (req, res) => {
     res.send("<h1>GameSorting index!</h1>");
