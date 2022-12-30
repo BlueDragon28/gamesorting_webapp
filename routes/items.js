fc68f13f08c0d243a93f02a5b53c0a794bf20d8d
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-12-30 17:32:08 +0100

    add new item form

--- routes/items.js
+++ routes/items.js
@@ -2,6 +2,32 @@ const database = require("../models/gameSortingDB");
 
 module.exports = (app) => {
     /*
+    Form to create a new item in a list in a collection
+    */
+    app.get("/collections/:collectionID/:listID/new", async (req ,res) => {
+        const { collectionID, listID } = req.params;
+
+        if (!await database.exists(database.COLLECTIONS, collectionID)) {
+            res.send("Invalid Collection");
+            return;
+        }
+
+        if (!await database.exists(database.LISTS, collectionID, listID)) {
+            res.send("Invalid List");
+            return;
+        }
+
+        const list = await database.find(database.LISTS, collectionID, listID);
+
+        if (!list) {
+            res.send("Cannot find list");
+            return;
+        }
+
+        res.render("collections/newItem", { list });
+    });
+
+    /*
     Display informations about an item
     */
     app.get("/collections/:collectionID/:listID/:itemID", async (req, res) => {
