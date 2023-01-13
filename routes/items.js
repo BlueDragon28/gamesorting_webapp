63aab4d6f6d01d75a86dc0840201ac0fff3be4ad
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-01-13 22:26:44 +0100

    item edit form: fill empty customData with empty object

--- routes/items.js
+++ routes/items.js
@@ -35,6 +35,61 @@ function parseCustomColumnsData(req, res, next) {
     next();
 }
 
+/*
+This function compare the raw customColumn with the available customDatas.
+The goal is to add empty customData if there is no value available.
+*/
+function addEmptyCustomDataIfNotFound(customDatasList, itemID, customColumn, customDatas) {
+    if (!customDatasList || !customColumn || !customDatas) {
+        throw new InternalError("Invalid Data");
+    }
+
+    for (let customData of customDatas) {
+        if (customData.ListColumnTypeID === customColumn.ListColumnTypeID) {
+            customDatasList.push(customData);
+            return;
+        }
+    }
+
+    customDatasList.push({
+        CustomRowItemsID: -1n,
+        ItemID: itemID,
+        ListColumnTypeID: customColumn.ListColumnTypeID,
+        Value: "",
+        ColumnName: customColumn.Name
+    });
+}
+
+/*
+Make sure the empty customDatas do not have the same negative value
+*/
+function fillNegativeValue(customDatas) {
+    let negativeValue = -1n;
+    for (let customData of customDatas) {
+        if (customData.CustomRowItemsID === -1n) {
+            customData.CustomRowItemsID = negativeValue--;
+        }
+    }
+}
+
+/*
+Compare raw customColumns with customData and add empty customData if there is no data
+*/
+function includeEmptyCustomColumnsData(itemData) {
+    if (!itemData) {
+        throw new InternalError("Invalid Item Data");
+    }
+
+    const parsedCustomDatas = [];
+    for (let customColumn of itemData.customColumns) {
+        addEmptyCustomDataIfNotFound(parsedCustomDatas, itemData.data.ItemID, customColumn, itemData.data.customData);
+    }
+
+    fillNegativeValue(parsedCustomDatas);
+
+    itemData.data.customData = parsedCustomDatas;
+}
+
 module.exports = (app) => {
     /*
     Form to create a new item in a list in a collection
@@ -72,11 +127,13 @@ module.exports = (app) => {
     app.get("/collections/:collectionID/:listID/:itemID/edit", wrapAsync(async (req, res) => {
         const { collectionID, listID, itemID } = req.params;
 
-        const item = await database.find(database.ITEMS, collectionID, listID, itemID);
+        let item = await database.find(database.ITEMS, collectionID, listID, itemID);
 
         if (!item) {
             throw new InternalError(`Failed To Query Item ${itemID}`);
         }
+
+        includeEmptyCustomColumnsData(item);
 
         res.render("collections/lists/items/edit", { item });
     }));
