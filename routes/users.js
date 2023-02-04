e90c2878f2d9a047f52e8b595c70c86093fa1f86
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-02-04 14:03:02 +0100

    add logout route

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,17 @@ router.get("/login", function(req, res) {
     res.render("login/index");
 });
 
+router.get("/logout", function(req, res) {
+    if (!req.session.user || !checkIfUserValid(req.session.user)) {
+        req.flash("success", "Already Logged Out");
+        return res.redirect("/");
+    }
+
+    req.session.user = null;
+    req.flash("success", "Successfully Logged Out");
+    res.redirect("/");
+});
+
 router.post("/register", wrapAsync(async function(req, res) {
     const { username, email, password } = req.body.user;
     const user = new User(username, email, password);
