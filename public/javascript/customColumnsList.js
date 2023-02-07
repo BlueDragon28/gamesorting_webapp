da9c91e48184d3c439117b5f886a248cd9c7e33f
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-02-07 17:12:25 +0100

    display columns type by adding them to the DOM

--- public/javascript/customColumnsList.js
+++ public/javascript/customColumnsList.js
@@ -1,1 +1,36 @@
 const columnsListDiv = document.querySelector("#columnsList");
+
+function parseIntTypeToHtml(columnType) {
+    if (!columnType.type || !columnType.min || !columnType.max) {
+        return "";
+    }
+    console.log(columnType)
+
+    return `Min: <b class="column-int-min">${columnType.min}</b>
+        Max: <b class="column-max-int">${columnType.max}</b>`;
+}
+
+function parseTypeToHtml(columnType) {
+    if (columnType.type === "@String") {
+        return "";
+    } else if (columnType.type === "@Int") {
+        return parseIntTypeToHtml(columnType);
+    }
+
+    return "";
+}
+
+for (let customColumn of listCustomColumns) {
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("card");
+    divContainer.innerHTML = `
+        <div class="card-body">
+        <p class="card-title">
+            Name: <b class="column-name">${customColumn.name}</b>
+            Type: <b class="column-type">${customColumn.type.type}</b>
+            ${parseTypeToHtml(customColumn.type)}
+        </p>
+        </div>
+    `;
+    columnsListDiv.append(divContainer);
+}
