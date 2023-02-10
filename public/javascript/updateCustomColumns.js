bdc7a2e699717967a4bfbd58f8f00890617b45d8
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-02-10 17:40:45 +0100

    customColumns: replace axios request with XMLHttpRequest

--- public/javascript/updateCustomColumns.js
+++ public/javascript/updateCustomColumns.js
@@ -3,14 +3,13 @@ console.log(newColumnsList);
 const submitButton = document.querySelector("#submit_button");
 
 submitButton.addEventListener("click", function() {
-    axios.post(`/collections/${list.parentCollection.id}/lists/${list.id}/custom-columns`, {
-            newColumns: newColumnsList.map(column => ({ id: column.id, name: column.name, type: column.type})),
-            columnsToDelete: removedColumn
-        })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    const columnsToAddAndDelete = {
+        newColumns: newColumnsList.map(column => ({ id: column.id, name: column.name, type: column.type})),
+        columnsToDelete: removedColumn
+    };
+
+    const xhrRequest = new XMLHttpRequest();
+    xhrRequest.open("POST", `/collections/${list.parentCollection.id}/lists/${list.id}/custom-columns`);
+    xhrRequest.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhrRequest.send(JSON.stringify(columnsToAddAndDelete));
 });
