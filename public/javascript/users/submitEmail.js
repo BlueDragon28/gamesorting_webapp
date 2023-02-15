1c68c1600ec6766d037210598fed8fead13cd771
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-02-15 22:06:17 +0100

    update email: display return message into the browser console

--- public/javascript/users/submitEmail.js
+++ public/javascript/users/submitEmail.js
@@ -1,11 +1,15 @@
+function whenProcessed(event) {
+    console.log(event.target.response);
+}
+
 export default function(email) {
     if (!email.trim().length) {
         return false;
     }
 
     const xhrRequest = new XMLHttpRequest();
-    //xhrRequest.addEventListener("load", onRequestFinish);
-    //xhrRequest.addEventListener("error", onRequestFinish);
+    xhrRequest.addEventListener("load", whenProcessed);
+    xhrRequest.addEventListener("error", whenProcessed);
     xhrRequest.open("POST", `/users/email?_method=PUT`);
     xhrRequest.setRequestHeader("Content-type", "application/json; charset=utf-8");
     xhrRequest.send(JSON.stringify({
