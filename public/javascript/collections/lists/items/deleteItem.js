b3bffadf1090545f44dbdcd109926785863404e1
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-02-20 17:13:04 +0100

    add delete list/item request javascripts files

---
+++ public/javascript/collections/lists/items/deleteItem.js
@@ -0,0 +1,36 @@
+import { makeAlertCard } from "../../../runtimeFlash/runtimeFlashHandler.js";
+
+const domModal = document.querySelector("#deleteItemModal");
+const deleteButtonModal = document.querySelector("#deleteItemButton");
+const bootstrapModal = new bootstrap.Modal(domModal);
+
+function hideModal() {
+    bootstrapModal.hide();
+}
+
+function onFinish(event) {
+    const response = JSON.parse(event.target.response);
+
+    if (response.type === "SUCCESS") {
+        window.location = `/collections/${item.parentList.parentCollection.id}/lists/${item.parentList.id}`;
+    } else {
+        makeAlertCard("error", "ERROR: " + response.message);
+        hideModal();
+    }
+}
+
+function onDelete() {
+    const xhrRequest = new XMLHttpRequest();
+    xhrRequest.addEventListener("load", onFinish);
+    xhrRequest.addEventListener("error", onFinish);
+    xhrRequest.open("POST", 
+        `/collections/${item.parentList.parentCollection.id}/lists/` +
+        `${item.parentList.id}/items/${item.id}?_method=DELETE`);
+    xhrRequest.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhrRequest.setRequestHeader("Accept", "application/json")
+    xhrRequest.send(JSON.stringify({
+        itemID: item.id
+    }));
+}
+
+deleteButtonModal.addEventListener("click", onDelete);
