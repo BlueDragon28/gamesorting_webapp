5cba2441e96fee2358a0fa8a886148ff2fd56ba5
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-03-03 12:21:53 +0100

    test bigint convertion

--- utils/numbers/bigint.test.js
+++ utils/numbers/bigint.test.js
@@ -9,6 +9,7 @@ test("Convert integer numbers bigint should work", function() {
 });
 
 test("Convert string numbers to bigint values should work", function() {
+    expect(bigint.toBigInt("")).toBe(0n);
     expect(bigint.toBigInt("0")).toBe(0n);
     expect(bigint.toBigInt("2")).toBe(2n);
     expect(bigint.toBigInt("1000")).toBe(1000n);
@@ -27,6 +28,13 @@ test("Convert NaN to bigint values should throw", function() {
     expect(() => bigint.toBigInt(NaN)).toThrow();
 });
 
+test("Convert invalid number string to bigint should throw", function() {
+    expect(() => bigint.toBigInt("qqdsqdf")).toThrow();
+    expect(() => bigint.toBigInt("1n")).toThrow();
+    expect(() => bigint.toBigInt("a")).toThrow();
+    expect(() => bigint.toBigInt("123 456")).toThrow();
+});
+
 test("Test if bigint are valid", function() {
     expect(bigint.isValid(0n)).toBe(true);
     expect(bigint.isValid(5n)).toBe(true);
@@ -34,3 +42,31 @@ test("Test if bigint are valid", function() {
     expect(bigint.isValid(1000n)).toBe(true);
     expect(bigint.isValid(-1000n)).toBe(true);
 });
+
+test("Test if integer numbers are valid bigint should work", function () {
+    expect(bigint.isValid(0)).toBe(true);
+    expect(bigint.isValid(5)).toBe(true);
+    expect(bigint.isValid(-5)).toBe(true);
+    expect(bigint.isValid(1000)).toBe(true);
+    expect(bigint.isValid(-1000)).toBe(true);
+});
+
+test("Test if string numbers are valid bigint should work", function() {
+    expect(bigint.isValid("0")).toBe(true);
+    expect(bigint.isValid("5")).toBe(true);
+    expect(bigint.isValid("-5")).toBe(true);
+    expect(bigint.isValid("1000")).toBe(true);
+    expect(bigint.isValid("-1000")).toBe(true);
+});
+
+test("Test if NaN is a valid bigint should be false", function() {
+    expect(bigint.isValid(NaN)).toBe(false);
+});
+
+test("Convert invalid number string to bigint should be false", function() {
+    expect(bigint.isValid("qqdsqdf")).toBe(false);
+    expect(bigint.isValid("1n")).toBe(false);
+    expect(bigint.isValid("a")).toBe(false);
+    expect(bigint.isValid("123 456")).toBe(false);
+});
+
