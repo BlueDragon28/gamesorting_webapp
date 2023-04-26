adbb7285a9ece417ac8b883eab3a2120ec6b18e6
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-04-26 21:44:45 +0200

    mariadb: load some value from file if necessary

--- sql/connection.js
+++ sql/connection.js
@@ -5,19 +5,53 @@ functions to make queries.
 
 const mariadb = require("mariadb");
 const { SqlError } = require("../utils/errors/exceptions");
+const { getEnvValueFromFile, isFileBased } = require("../utils/loadingEnvVariable");
 
 let pool = undefined;
 
-let databaseName = process.env.MARIADB_DATABASE_NAME;
+const dbConnectionInfo = {
+    user: undefined,
+    host: undefined,
+    port: undefined,
+    socketPath: undefined,
+    database: undefined,
+    connectionLimit: 1,
+    multipleStatements: true
+};
+
+if (typeof process.env.MARIADB_USER !== "string" && !process.env.MARIADB_USER.length) {
+    throw new Error("Database user username must be provided with the MARIADB_USER env variable!");
+}
+dbConnectionInfo.user = isFileBased(process.env.MARIADB_USER) ?
+    getEnvValueFromFile(process.env.MARIADB_USER) : process.env.MARIADB_USER;
+
+if (typeof process.env.MARIADB_HOST_PORT === "string" && process.env.MARIADB_HOST_PORT.length) {
+    const [host,port] = 
+        (isFileBased(process.env.MARIADB_HOST_PORT) ? 
+            getEnvValueFromFile(process.env.MARIADB_HOST_PORT) : 
+            process.env.MARIADB_HOST_PORT)
+        .split(":").map((item, index) => index === 1 ? Number.parseInt(item) : item);
+    
+    dbConnectionInfo.host = host;
+    dbConnectionInfo.port = port;
+} else if (typeof process.env.MARIADB_SOCKET_PATH === "string" && process.env.MARIADB_SOCKET_PATH.length) {
+    dbConnectionInfo.socketPath = process.env.MARIADB_SOCKET_PATH;
+} else {
+    throw new Error("You must provide MariaDB user and host or socket path connection info!");
+}
+
+if (typeof process.env.MARIADB_DATABASE_NAME === "string" && process.env.MARIADB_DATABASE_NAME.length) {
+    dbConnectionInfo.database = process.env.MARIADB_DATABASE_NAME;
+} else {
+    throw new Error("You must provide database name!");
+}
+
+if (typeof process.env.MARIADB_CONNECTION_LIMIT === "string" && process.env.MARIADB_CONNECTION_LIMIT.length) {
+    dbConnectionInfo.connectionLimit = process.env.MARIADB_CONNECTION_LIMIT;
+}
 
 function openPool(suffix = "") {
-    pool = mariadb.createPool({
-        user: process.env.MARIADB_USER,
-        socketPath: process.env.MARIADB_SOCKET_PATH,
-        database: process.env.NODE_ENV === "production" ? databaseName : databaseName + suffix,
-        connectionLimit: process.env.MARIADB_CONNECTION_LIMIT,
-        multipleStatements: true
-    });
+    pool = mariadb.createPool(dbConnectionInfo);
 }
 
 module.exports = {
