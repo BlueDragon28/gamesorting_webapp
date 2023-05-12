bf13ec26510ed5636d8367e017f53fd13f8daccb
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-05-12 15:50:52 +0200

    read email variable from encrypted files

--- utils/email/email.js
+++ utils/email/email.js
@@ -1,20 +1,47 @@
 const nodemailer = require("nodemailer");
+const { isFileBased, getEnvValueFromFile } = require("../loadingEnvVariable");
 
-const senderEmail = process.env.EMAIL_SENDER;
+let emailHost;
+let emailUser;
+let emailPassword;
+let emailSender;
+
+if (process.env.NODE_ENV !== "production") {
+    emailHost = process.env.EMAIL_HOST;
+    emailUser = process.env.EMAIL_USER;
+    emailPassword = process.env.EMAIL_PASS;
+    emailSender = process.env.EMAIL_SENDER;
+} else {
+    emailHost = isFileBased(process.env.EMAIL_HOST) ?
+        getEnvValueFromFile(process.env.EMAIL_HOST) :
+        process.env.EMAIL_HOST;
+    
+    emailUser = isFileBased(process.env.EMAIL_USER) ?
+        getEnvValueFromFile(process.env.EMAIL_USER) :
+        process.env.EMAIL_USER;
+    
+    emailPassword = isFileBased(process.env.EMAIL_PASS) ?
+        getEnvValueFromFile(process.env.EMAIL_PASS) :
+        process.env.EMAIL_PASS;
+    
+    emailSender = isFileBased(process.env.EMAIL_SENDER) ?
+        getEnvValueFromFile(process.env.EMAIL_SENDER) :
+        process.env.EMAIL_SENDER;
+}
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    host: emailHost,
     port: Number(process.env.EMAIL_PORT),
     secure: process.env.EMAIL_SECURE === "true" ? true : false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: emailUser,
+        pass: emailPassword
     }
 });
 
 async function sendLostPasswordEmail(to, url) {
     return transporter.sendMail({
-        from: `"GameSorting" <${senderEmail}>`,
+        from: `"GameSorting" <${emailSender}>`,
         to,
         subject: "Lost password",
         text: "Click here to reset your password!\nThis link will only be valid for the next 2 hours.\nThis email was sent automatically, do not anwser it.",
@@ -24,6 +51,6 @@ async function sendLostPasswordEmail(to, url) {
 
 module.exports = {
     transporter,
-    senderEmail,
+    emailSender,
     sendLostPasswordEmail
 };
