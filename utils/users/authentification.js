57b139756eac2e34cc97d22b0acbbeda7060e8e0
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-05-22 17:40:50 +0200

    add middleware to check if user password is valid

--- utils/users/authentification.js
+++ utils/users/authentification.js
@@ -1,4 +1,7 @@
 const { checkIfUserValid } = require("../validation/users");
+const { existingOrNewConnection } = require("../sql/sql");
+const { User } = require("../../models/users");
+const wrapAsync = require("../errors/wrapAsync");
 
 /*
 Check if the user is logged in. If not, redirect him to the loggin page
@@ -12,6 +15,41 @@ function isLoggedIn(req, res, next) {
     next();
 }
 
-module.exports = {
-    isLoggedIn
+async function isUserPasswordValid(req, res, next) {
+    const { password, currentPassword } = req.body;
+
+    const userPassword = typeof password === "string" ? password : typeof currentPassword === "string" ? currentPassword : undefined;
+
+    if (typeof userPassword !== "string") {
+        return next({ statusCode: 404, message: "Invalid password!" });
+    }
+
+    const [success, error] = await existingOrNewConnection(null, async function(connection) {
+        try {
+            const foundUser = await User.findByID(req.session.user.id, connection);
+
+            if (!foundUser || !foundUser instanceof User || !foundUser.isValid()) {
+                return [false, { statusCode: 404, message: "User Not Found!" }];
+            }
+
+            if (!foundUser.compare(foundUser.email, userPassword)) {
+                return [false, { statusCode: 404, message: "Invalid password" }];
+            }
+        } catch (error) {
+            return [false, { statusCode: 500, message: error.message }];
+        }
+
+        return [true, null];
+    });
+
+    if (!success) {
+        return next(error);
+    }
+
+    next();
 }
+
+module.exports = {
+    isLoggedIn,
+    isUserPasswordValid: wrapAsync(isUserPasswordValid)
+};
