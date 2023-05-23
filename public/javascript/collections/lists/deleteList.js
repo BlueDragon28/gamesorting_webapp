72745d2e57f02c31b275fb9635ab65ce0e462553
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-05-23 15:10:23 +0200

    modify delete list modal to request user password

--- public/javascript/collections/lists/deleteList.js
+++ public/javascript/collections/lists/deleteList.js
@@ -1,11 +1,26 @@
 import { makeAlertCard } from "../../runtimeFlash/runtimeFlashHandler.js";
+import { hideError, setError } from "../../users/userModalErrorCard.1.0.0.js";
 
 const domModal = document.querySelector("#deleteListModal");
 const deleteButtonModal = document.querySelector("#deleteListButton");
 const bootstrapModal = new bootstrap.Modal(domModal);
 
+const userPasswordInput = document.getElementById("deleteListUserPassword");
+const openModalButton = document.getElementById("open-delete-list-modal-button");
+
 function hideModal() {
     bootstrapModal.hide();
+}
+
+function resetInput() {
+    userPasswordInput.value = "";
+}
+
+function openModal() {
+    resetInput();
+    hideError();
+
+    bootstrapModal.show();
 }
 
 function onFinish(event) {
@@ -15,11 +30,26 @@ function onFinish(event) {
         window.location = `/collections/${list.parentCollection.id}/`;
     } else {
         makeAlertCard("error", "ERROR: " + response.message);
-        hideModal();
+    }
+}
+
+function getPassword() {
+    const userPassword = userPasswordInput.value;
+
+    if (!userPassword || !userPassword.length) {
+        return [false, null];
+    } else {
+        return [true, userPassword];
     }
 }
 
 function onDelete() {
+    const [success, userPassword] = getPassword();
+
+    if (!success) {
+        return setError("Password cannot be empty");
+    }
+
     const xhrRequest = new XMLHttpRequest();
     xhrRequest.addEventListener("load", onFinish);
     xhrRequest.addEventListener("error", onFinish);
@@ -27,8 +57,12 @@ function onDelete() {
     xhrRequest.setRequestHeader("Content-type", "application/json; charset=utf-8");
     xhrRequest.setRequestHeader("Accept", "application/json")
     xhrRequest.send(JSON.stringify({
-        listID: list.id
+        listID: list.id,
+        password: userPassword
     }));
+
+    hideModal();
 }
 
 deleteButtonModal.addEventListener("click", onDelete);
+openModalButton.addEventListener("click", openModal);
