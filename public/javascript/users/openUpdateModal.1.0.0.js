6af61edb4e76986af15d369aca8d0ca9e64db4b4
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-06-02 16:34:59 +0200

    add openUpdateModel js to webpack bundle js file

--- public/javascript/users/openUpdateModal.1.0.0.js
+++ public/javascript/users/openUpdateModal.1.0.0.js
@@ -3,19 +3,30 @@ import submitPassword from "./submitPassword.1.0.0.js";
 import { hideError, setError } from "./userModalErrorCard.1.0.0.js";
 
 (function openUpdateModelOnUserRequest() {
-    const modalTitle = document.querySelector("#modal-title");
-    const emailUpdateDiv = document.querySelector("#email-update-input");
-    const passwordUpdateDiv = document.querySelector("#password-update-input");
-    const emailChangeButton = document.querySelector("#open-email-change-modal");
-    const passwordChangeButton = document.querySelector("#open-password-change-modal");
-    const emailSubmitButton = document.querySelector("#button-submit-new-email");
-    const passwordSubmitButton = document.querySelector("#button-submit-new-password");
+    const modalTitle = document.getElementById("modal-title");
+    const emailUpdateDiv = document.getElementById("email-update-input");
+    const passwordUpdateDiv = document.getElementById("password-update-input");
+    const emailChangeButton = document.getElementById("open-email-change-modal");
+    const passwordChangeButton = document.getElementById("open-password-change-modal");
+    const emailSubmitButton = document.getElementById("button-submit-new-email");
+    const passwordSubmitButton = document.getElementById("button-submit-new-password");
     
-    const inputEmail = document.querySelector("#input-email");
-    const inputPassword = document.querySelector("#input-password");
-    const inputCurrentPassword = document.querySelector("#current-password");
-    const inputNewPassword = document.querySelector("#new-password");
-    const inputRetypeNewPassword = document.querySelector("#retype-new-password");
+    const inputEmail = document.getElementById("input-email");
+    const inputPassword = document.getElementById("input-password");
+    const inputCurrentPassword = document.getElementById("current-password");
+    const inputNewPassword = document.getElementById("new-password");
+    const inputRetypeNewPassword = document.getElementById("retype-new-password");
+
+    const domModal = document.getElementById("updateContentModal");
+
+    if (!modalTitle || !emailUpdateDiv || !passwordUpdateDiv ||
+        !emailChangeButton || !passwordChangeButton ||
+        !emailSubmitButton || !passwordSubmitButton ||
+        !inputEmail || !inputPassword || !inputCurrentPassword || 
+        !inputNewPassword || !inputRetypeNewPassword) {
+
+        return;
+    }
 
     let modal;
 
@@ -31,7 +42,7 @@ import { hideError, setError } from "./userModalErrorCard.1.0.0.js";
         resetInputs();
         hideError();
 
-        modal = new bootstrap.Modal(document.querySelector("#updateContentModal"));
+        modal = new bootstrap.Modal(domModal);
         modal.show();
     }
 
--- public/javascript/webpackEntry.js
+++ public/javascript/webpackEntry.js
@@ -1,4 +1,5 @@
 import "./checkFormValidation";
 import "./runtimeFlash/serverFlashes";
 import "./paginations/setPaginationsHRef";
+import "./users/openUpdateModal.1.0.0";
 import "./users/deleteUserModal.1.0.0";
