7e2a17ef5406ed9b4f7fc3d255e48ec852d0d194
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-06-02 13:21:20 +0200

    add basic weback entry

---
+++ webpack.development.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./public/javascript/webpackEntry.js",
+    output: {
+        path: path.resolve(__dirname, "public/dist"),
+        filename: "public-js.bundle.1.0.0.js"
+    },
+    resolve: {
+        roots: [path.resolve(__dirname, "public")]
+    },
+    devtool: false
+}
