85d6e8c9b7acc82d6cf595cc29c5c6bbe8f2e0be
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-07-29 22:36:35 +0200

    Add ask confirmation modal to import custom columns page

--- public/javascript/customColumns/importCustomColumns.js
+++ public/javascript/customColumns/importCustomColumns.js
@@ -1,13 +1,26 @@
+//import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { makeAlertCard } from "../runtimeFlash/runtimeFlashHandler.js";
 
 (function() {
     const searchedListUl = document.getElementById("search-lists-ul");
     const searchListForm = document.getElementById("search-lists-form");
+    const DOMConfirmModal = document.getElementById("search-lists-accept-modal");
+    const modalListNameSpan = document.getElementById("search-lists-accept-modal-list-name");
+    const modalAcceptButton = document.getElementById("search-lists-accept-modal-accept-button");
+
     let selectedID = null;
 
-    if (!searchedListUl || !searchListForm) {
+    if (
+        !searchedListUl || 
+        !searchListForm ||
+        !DOMConfirmModal ||
+        !modalListNameSpan ||
+        !modalAcceptButton
+    ) {
         return;
     }
+
+    const confirmModal = new bootstrap.Modal(DOMConfirmModal);
 
     const buttonsListSelector = searchedListUl.children;
 
@@ -41,8 +54,23 @@ import { makeAlertCard } from "../runtimeFlash/runtimeFlashHandler.js";
             makeAlertCard("error", "You must select a list to import");
             return;
         }
+
+        for (const searchedList of searchedLists) {
+            if (searchedList.id === selectedID) {
+                modalListNameSpan.innerText = 
+                    `${searchedList.parentCollection.name}/${searchedList.name}`;
+                break;
+            }
+        }
+
+        confirmModal.show();
         event.preventDefault();
     }
 
+    function onUserAccept() {
+        console.log("user accepted");
+    }
+
     searchListForm.addEventListener("submit", onFormSubmit);
+    modalAcceptButton.addEventListener("click", onUserAccept);
 })();
