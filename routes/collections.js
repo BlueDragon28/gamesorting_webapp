80831e265ad1de9440a31f7b5117ca6abaa81226
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-08-23 15:10:31 +0200

    Provide basic validation to new collection/list form

--- routes/collections.js
+++ routes/collections.js
@@ -179,7 +179,38 @@ router.get("/lists/:listID/item/:itemID", wrapAsync(async function(req, res) {
             originalUrl: req.originalUrl
         });
     }
-}))
+}));
+
+router.post("/lists", wrapAsync(async function(req, res) {
+    const { collection_list_name: collectionListName } = req.body;
+    let [collectionName,listName] = collectionListName.split("/");
+    collectionName = collectionName?.trim();
+    listName = listName?.trim();
+
+    let errorMessage = undefined;
+
+    if (typeof collectionListName !== "string" || !collectionListName.length) {
+        errorMessage = "You must provide a collection and list name";
+    } else if (collectionListName.indexOf("/") === -1) {
+        errorMessage = "You must seperate collection and list name with a slash /";
+    } else if (collectionListName.indexOf("/") !== collectionListName.lastIndexOf("/")) {
+        errorMessage = "You should not put multiple slash /";
+    } else if (typeof collectionName !== "string" || !collectionName.length) {
+        errorMessage = "You must provide a collection name to the left of the slash /";
+    } else if (collectionName.length < 3) {
+        errorMessage = "Collection name must be at least 3 characters";
+    } else if (typeof listName !== "string" || !listName.length) {
+        errorMessage = "You must provide a list name to the right of the slash /";
+    } else if (listName.length < 3) {
+        errorMessage = "List name must be at least 3 characters";
+    }
+
+    res.render("partials/htmx/collections/new_collection_list_form", {
+        errorMessage,
+        justValidation: true,
+        inputValue: collectionListName,
+    });
+}));
 
 /*
 Validate collectionID on each route asking for collection id
