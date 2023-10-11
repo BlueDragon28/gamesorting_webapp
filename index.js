a061f56668e95e5e098707f91d038136622cebe4
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-10-11 17:35:13 +0200

    Remove parse flash middleware

--- index.js
+++ index.js
@@ -87,7 +87,6 @@ app.use(session({
     }
 }));
 app.use(flash());
-app.use(parseFlashMessage);
 app.use(function(req, res, next) {
     res.locals.currentUser = req.session.user;
     res.locals.activeLink = "";
