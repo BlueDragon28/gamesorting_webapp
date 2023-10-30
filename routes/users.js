7dc8c3613b9fb0ba3385c98d1495d0e0c1944b72
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-10-30 18:40:18 +0100

    Display delete user account modal when clicking delete button

--- routes/users.js
+++ routes/users.js
@@ -469,6 +469,27 @@ router.post("/update-password", wrapAsync(async function(req, res) {
     }
 }));
 
+router.get("/delete-modal", wrapAsync(async function(req, res) {
+    if (!req.session.user) {
+        return htmxRedirect(req, res, "/");
+    }
+
+    const userID = req.session.user.id;
+
+    const foundUser = await User.findByID(userID);
+    if (!foundUser || !(foundUser instanceof User) || !foundUser.isValid()) {
+        req.flash("error", "Failed to found user");
+        return res.set({
+            "HX-Trigger": "new-flash-event",
+        }).status(204).send();
+    }
+
+    res.locals.activeLink = "UserInformations";
+    res.render("partials/htmx/modals/deleteUserModal.ejs", {
+        user: foundUser,
+    });
+}));
+
 router.post("/lostpassword", wrapAsync(async function(req, res) {
     throw new InternalError("Not yet implemented");
     const { email } = req.body;
