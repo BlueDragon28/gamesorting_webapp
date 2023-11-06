04d968aac318c630b7cc2c92ce9daea57f8c27ed
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-11-06 13:48:30 +0100

    fix exception thrown on modal content observer function

--- public/javascript/htmx/open-modal-when-mounted.js
+++ public/javascript/htmx/open-modal-when-mounted.js
@@ -2,7 +2,10 @@ function mutationObserver(mutationList, observer) {
     for (const mutation of mutationList) {
         if (mutation.type === "childList") {
             for (const element of mutation.addedNodes) {
-                if (element.getAttribute("gs-new-modal") === "true") {
+                if (
+                    element.getAttribute &&
+                    element.getAttribute("gs-new-modal") === "true"
+                ) {
                     const modal = bootstrap.Modal.getOrCreateInstance(element);
                     modal.show();
                     element.removeAttribute("gm-new-modal");
