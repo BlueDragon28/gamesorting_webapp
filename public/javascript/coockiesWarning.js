5d83695b1d777e92c2536c4b732bd043dc377ed7
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2023-11-13 15:17:15 +0100

    If user haven't accept cookied, display a modal to ask is permission

--- public/javascript/coockiesWarning.js
+++ public/javascript/coockiesWarning.js
@@ -1,9 +1,3 @@
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
-const warningDomModal = document.getElementById("cookiesWarningModal");
-const acceptButton = document.getElementById("accept-essential-cookies-button");
-
-const bootstrapModal = new bootstrap.Modal(warningDomModal);
-
 function doesUserAlreadyAcceptedCookies() {
     const userChoice = localStorage.getItem("acceptEssentialCookies");
 
@@ -18,7 +12,48 @@ function userAccepted() {
 (function openModalIfNeeded() {
     if (doesUserAlreadyAcceptedCookies()) return;
 
-    bootstrapModal.show();
-})();
+    /*
+    * Run execution on next tick to be sure bootstrap had time to initialize.
+    */
+    setTimeout(function() {
+        const modalSection = document.getElementById("modal-content-section");
 
-acceptButton.addEventListener("click", userAccepted);
+        const modalElement = document.createElement("div");
+        modalElement.id = "accept-essential-cookies-modal";
+        modalElement.classList.add("modal", "fade");
+        modalElement.setAttribute("tabindex", "-1");
+        modalElement.setAttribute("aria-labelledby", "acceptEssentialCookiesModalLabel");
+        modalElement.setAttribute("aria-hidden", "true");
+        modalElement.setAttribute("data-bs-backdrop", "static");
+        modalElement.setAttribute("data-bs-keyboard", "false");
+        modalElement.innerHTML = `
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5">This Web App Use Cookies</h1>
+        </div>
+        <div class="modal-body">
+            <p>This web app use only cookies that are essential to work properly.<br>
+                It may also store some data into the local storage of your web browsers (including your anwser to this dialog).</p>
+        </div>
+        <div class="modal-footer">
+            <button class="btn btn-outline-secondary" type="button" id="accept-essential-cookies-button">Accept Essential Cookies</button>
+        </div>
+    </div>
+</div>
+`;
+
+        modalSection.append(modalElement);
+        const modal = bootstrap.Modal.getOrCreateInstance(modalElement);
+        modal.show();
+
+        modalElement.querySelector("#accept-essential-cookies-button")?.addEventListener(
+            "click",
+            function(_) {
+                modal.hide();
+                modalElement.remove();
+                userAccepted();
+            },
+        );
+    }, 0);
+})();
