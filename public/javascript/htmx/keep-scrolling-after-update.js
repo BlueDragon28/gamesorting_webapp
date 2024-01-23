998e803b2776e6bbe249f46ba06512330e889443
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2024-01-23 19:00:53 +0100

    keep track of multiple scroll position at the same time

--- public/javascript/htmx/keep-scrolling-after-update.js
+++ public/javascript/htmx/keep-scrolling-after-update.js
@@ -3,38 +3,42 @@ const KEEP_SCROLL_RESTORED = "gs-keep-scroll-restored";
 
 export function restoreScrollPosition(element) {
   // Check if the element or child element has the keep scroll attribute set
-  const listGroupElement = element.hasAttribute(KEEP_SCROLL_ATTRIBUTE_NAME)
+  const listGroupElements = element.hasAttribute(KEEP_SCROLL_ATTRIBUTE_NAME)
     ? element
-    : element.querySelector(
-        `[${KEEP_SCROLL_ATTRIBUTE_NAME}]:not([${KEEP_SCROLL_RESTORED}="true"]`,
+    : element.querySelectorAll(
+        `[${KEEP_SCROLL_ATTRIBUTE_NAME}]:not(.${KEEP_SCROLL_RESTORED})`,
       );
 
-  if (
-    listGroupElement &&
-    !listGroupElement.hasAttribute(KEEP_SCROLL_RESTORED)
-  ) {
-    // Get the scroll position base on the attribute value
-    const scrollValue = listGroupElement.getAttribute(
-      KEEP_SCROLL_ATTRIBUTE_NAME,
-    );
-    if (typeof scrollValue !== "string" || !scrollValue.length) return;
+  listGroupElements.forEach((listGroupElement) => {
+    if (
+      listGroupElement &&
+      !listGroupElement.classList.contains(KEEP_SCROLL_RESTORED)
+    ) {
+      // Get the scroll position base on the attribute value
+      const scrollValue = listGroupElement.getAttribute(
+        KEEP_SCROLL_ATTRIBUTE_NAME,
+      );
+      if (typeof scrollValue !== "string" || !scrollValue.length) return;
 
-    const scrollPosition = sessionStorage.getItem(scrollValue);
+      const scrollPosition = sessionStorage.getItem(scrollValue);
 
-    // restore scroll position
-    listGroupElement.scrollTop = parseInt(scrollPosition) || 0;
+      // restore scroll position
+      listGroupElement.scrollTop = parseInt(scrollPosition) || 0;
 
-    // add attribute to indicate the the scroll has been restored
-    listGroupElement.setAttribute(KEEP_SCROLL_RESTORED, "true");
+      // add attribute to indicate the the scroll has been restored
+      listGroupElement.classList.add(KEEP_SCROLL_RESTORED);
 
-    // Add event handler to save scroll position
-    listGroupElement.addEventListener("scrollend", function (event) {
-      const scrollValue = event.target.getAttribute(KEEP_SCROLL_ATTRIBUTE_NAME);
-      const scrollPosition = event.target.scrollTop;
+      // Add event handler to save scroll position
+      listGroupElement.addEventListener("scrollend", function (event) {
+        const scrollValue = event.target.getAttribute(
+          KEEP_SCROLL_ATTRIBUTE_NAME,
+        );
+        const scrollPosition = event.target.scrollTop;
 
-      if (!scrollValue) return;
+        if (!scrollValue) return;
 
-      sessionStorage.setItem(scrollValue, scrollPosition.toString());
-    });
-  }
+        sessionStorage.setItem(scrollValue, scrollPosition.toString());
+      });
+    }
+  });
 }
