21c92e48e12dc6e114105ee855d8b6f3961d19d5
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2024-01-24 15:45:19 +0100

    set dist js/css to v2.1.0

--- public.javascript.webpack.development.config.js
+++ public.javascript.webpack.development.config.js
@@ -3,32 +3,32 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    entry: "./public/javascript/webpackEntry.js",
-    output: {
-        path: path.resolve(__dirname, "public/dist"),
-        filename: "public-js.bundle.2.0.1.js"
-    },
-    resolve: {
-        roots: [path.resolve(__dirname, "public"), path.resolve(__dirname, "node_modules")]
-    },
-    devtool: false,
-    module: {
-        rules: [
-            {
-                test: /.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            `...`,
-            new CssMinimizerPlugin()
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "app.2.0.1.css"
-        })
-    ]
-}
+  entry: "./public/javascript/webpackEntry.js",
+  output: {
+    path: path.resolve(__dirname, "public/dist"),
+    filename: "public-js.bundle.2.1.0.js",
+  },
+  resolve: {
+    roots: [
+      path.resolve(__dirname, "public"),
+      path.resolve(__dirname, "node_modules"),
+    ],
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "app.2.1.0.css",
+    }),
+  ],
+};
