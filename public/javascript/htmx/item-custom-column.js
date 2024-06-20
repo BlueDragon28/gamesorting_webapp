10e8bdd8424cac917a440ad95a44ad5d3986bd70
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2024-06-20 21:17:05 +0200

    add close btn to remove custom row on items

--- public/javascript/htmx/item-custom-column.js
+++ public/javascript/htmx/item-custom-column.js
@@ -1,12 +1,39 @@
 const CUSTOM_COLUMN_ATT_NAME = "gs-new-custom-column-data";
 const CUSTOM_COLUMN_BTN = "btn";
 const CUSTOM_COLUMN_LIST = "list";
+const CUSTOM_COLUMN_DELETE = "delete";
+const CUSTOM_COLUMN_BLOCK = "inputBlock";
+
+document.body.addEventListener("click", function (evt) {
+  if (
+    !evt.target ||
+    evt.target?.getAttribute(CUSTOM_COLUMN_ATT_NAME) !== CUSTOM_COLUMN_DELETE
+  ) {
+    return;
+  }
+
+  const parentBlock = evt.target?.parentElement?.parentElement;
+
+  if (
+    !parentBlock ||
+    !parentBlock.getAttribute(CUSTOM_COLUMN_ATT_NAME) === CUSTOM_COLUMN_BLOCK
+  ) {
+    return;
+  }
+
+  parentBlock.parentElement?.removeChild(parentBlock);
+});
 
 function addNewCustomField(selectedList) {
   selectedList.innerHTML += `
-    <div>
-      <input type="text" class="form-control form-control-sm border-bottom-0 rounded-top rounded-bottom-0 mt-2" placeholder="Name" />
-      <input type="text" class="form-control border-top-0 rounded-top-0 rounded-bottom" placeholder="Value" />
+    <div class="d-flex flex-row" ${CUSTOM_COLUMN_ATT_NAME}="${CUSTOM_COLUMN_BLOCK}">
+      <div class="flex-fill">
+        <input type="text" class="form-control form-control-sm border-bottom-0 rounded-top rounded-bottom-0 mt-2" placeholder="Name" />
+        <input type="text" class="form-control border-top-0 rounded-top-0 rounded-bottom" placeholder="Value" />
+      </div>
+      <div class="d-flex flex-column justify-content-center">
+        <button type="button" class="btn-close my-auto" ${CUSTOM_COLUMN_ATT_NAME}="${CUSTOM_COLUMN_DELETE}" />
+      <div>
     </div>
   `;
 }
@@ -25,7 +52,7 @@ export function addListenerItemCustomColumn(element) {
 
   if (
     !selectList ||
-    !selectList.getAttribute(CUSTOM_COLUMN_ATT_NAME) === CUSTOM_COLUMN_LIST
+    selectList.getAttribute(CUSTOM_COLUMN_ATT_NAME) !== CUSTOM_COLUMN_LIST
   ) {
     return;
   }
