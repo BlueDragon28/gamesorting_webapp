6691f672bf073755573bc0d03af6577d0bc00bb5
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2024-06-21 07:48:25 +0200

    apply prettier to items.js and customDataValidation.js

--- utils/validation/customDataValidation.js
+++ utils/validation/customDataValidation.js
@@ -9,147 +9,188 @@ const mariadb = require("../../sql/connection");
 const { ValueError, InternalError } = require("../errors/exceptions");
 const wrapAsync = require("../errors/wrapAsync");
 
-async function getType(customColumns) {   
-    if (!customColumns || !Array.isArray(customColumns)) {
+async function getType(customColumns) {
+  if (!customColumns || !Array.isArray(customColumns)) {
+    throw new ValueError(400, "Invalid Custom Data Value");
+  }
+
+  return await mariadb.getConnection(async function (connection) {
+    for (let customColumn of customColumns) {
+      if (
+        !customColumn &&
+        (!customColumn.ListColumnTypeID || !customColumn.CustomRowItemsID) &&
+        !customColumn.Value
+      ) {
         throw new ValueError(400, "Invalid Custom Data Value");
+      }
+
+      let columnType = [];
+
+      if (customColumn.ListColumnTypeID) {
+        columnType = await ListColumnType.findByID(
+          customColumn.ListColumnTypeID,
+          connection
+        );
+      } else if (
+        customColumn.CustomRowItemsID &&
+        customColumn.CustomRowItemsID >= 0
+      ) {
+        columnType = await ListColumnType.findFromUserData(
+          customColumn.CustomRowItemsID,
+          connection
+        );
+      } else if (customColumn.CustomRowItemsID) {
+        columnType = await ListColumnType.findByID(
+          -customColumn.CustomRowItemsID,
+          connection
+        );
+      } else {
+        throw new ValueError(400, "Invalid Custom Data Value");
+      }
+
+      customColumn.columnType = columnType.Type || columnType.type;
+      customColumn.baseListColumnTypeID = columnType.ListColumnTypeID;
     }
 
-    return await mariadb.getConnection(async function(connection) {
-        for (let customColumn of customColumns) {
-            if (!customColumn && ((!customColumn.ListColumnTypeID || !customColumn.CustomRowItemsID) &&
-                !customColumn.Value)) {
-                throw new ValueError(400, "Invalid Custom Data Value");
-            }
-
-            let columnType = [];
-
-            if (customColumn.ListColumnTypeID) {
-                columnType = await ListColumnType.findByID(customColumn.ListColumnTypeID, connection);
-            } else if (customColumn.CustomRowItemsID && customColumn.CustomRowItemsID >= 0) {
-                columnType = await ListColumnType.findFromUserData(customColumn.CustomRowItemsID, connection);
-            } else if (customColumn.CustomRowItemsID) {
-                columnType = await ListColumnType.findByID(-customColumn.CustomRowItemsID, connection);
-            } else {
-                throw new ValueError(400, "Invalid Custom Data Value");
-            }
-
-            customColumn.columnType = columnType.Type || columnType.type;
-            customColumn.baseListColumnTypeID = columnType.ListColumnTypeID;
-        }
-
-        return customColumns;
-    });
+    return customColumns;
+  });
 }
 
 /*
 Validate the custom data with a column type of string
 */
 function makeCustomDataStringValidation() {
-    return Joi.object({
-        Value: Joi.alternatives().try(
-            Joi.string().trim().max(300, "utf8").min(1, "utf8").forbidHTML(),
-            Joi.string().trim().min(0).max(0)
-        ),
-        columnType: Joi.object({
-            type: Joi.string().pattern(/@String/).required()
-        }).unknown().required()
-    }).unknown().required();
+  return Joi.object({
+    Value: Joi.alternatives().try(
+      Joi.string().trim().max(300, "utf8").min(1, "utf8").forbidHTML(),
+      Joi.string().trim().min(0).max(0)
+    ),
+    columnType: Joi.object({
+      type: Joi.string()
+        .pattern(/@String/)
+        .required(),
+    })
+      .unknown()
+      .required(),
+  })
+    .unknown()
+    .required();
 }
 
 function makeCustomDataHRefValidation() {
-    return Joi.object({
-        Value: Joi.alternatives().try(
-            Joi.string().trim().max(300, "utf8").min(1, "utf8").forbidHTML().uri({
-                scheme: [
-                    "http",
-                    "https"
-                ]
-            }),
-            Joi.string().trim().min(0).max(0)
-        ),
-        columnType: Joi.object({
-            type: Joi.string().pattern(/@Href/).required()
-        }).unknown().required()
-    }).unknown().required();
+  return Joi.object({
+    Value: Joi.alternatives().try(
+      Joi.string()
+        .trim()
+        .max(300, "utf8")
+        .min(1, "utf8")
+        .forbidHTML()
+        .uri({
+          scheme: ["http", "https"],
+        }),
+      Joi.string().trim().min(0).max(0)
+    ),
+    columnType: Joi.object({
+      type: Joi.string().pattern(/@Href/).required(),
+    })
+      .unknown()
+      .required(),
+  })
+    .unknown()
+    .required();
 }
 
 /*
 Validate the custom data with a column type of int
 */
 function makeCustomDataNumberValidation() {
-    return Joi.object({
-        Value: Joi.alternatives().try(
-            Joi.number().min(Joi.ref("columnType.min")).max(Joi.ref("columnType.max")),
-            Joi.string().trim().min(0).max(0)
-        ),
-        columnType: Joi.object({
-            type: Joi.string().pattern(/@Int/).required(),
-            min: Joi.number(),
-            max: Joi.number()
-        }).unknown().required()
-    }).unknown().required();
+  return Joi.object({
+    Value: Joi.alternatives().try(
+      Joi.number()
+        .min(Joi.ref("columnType.min"))
+        .max(Joi.ref("columnType.max")),
+      Joi.string().trim().min(0).max(0)
+    ),
+    columnType: Joi.object({
+      type: Joi.string().pattern(/@Int/).required(),
+      min: Joi.number(),
+      max: Joi.number(),
+    })
+      .unknown()
+      .required(),
+  })
+    .unknown()
+    .required();
 }
 
 function makeCustomDataStarsValidation() {
-    return Joi.object({
-        Value: Joi.number().min(0).max(5),
-        columnType: Joi.object({
-            type: Joi.string().pattern(/@Stars/).required()
-        }).unknown().required()
-    }).unknown().required();
+  return Joi.object({
+    Value: Joi.number().min(0).max(5),
+    columnType: Joi.object({
+      type: Joi.string()
+        .pattern(/@Stars/)
+        .required(),
+    })
+      .unknown()
+      .required(),
+  })
+    .unknown()
+    .required();
 }
 
 function columnDataAndTypeValidation() {
-    return Joi.alternatives().try(
-        makeCustomDataStringValidation(),
-        makeCustomDataHRefValidation(),
-        makeCustomDataNumberValidation(),
-        makeCustomDataStarsValidation()
-    );
+  return Joi.alternatives().try(
+    makeCustomDataStringValidation(),
+    makeCustomDataHRefValidation(),
+    makeCustomDataNumberValidation(),
+    makeCustomDataStarsValidation()
+  );
 }
 
 function validate() {
-    const celebrateValidation = {
-        [Segments.BODY]: Joi.object({
-            customColumn: Joi.array().items(columnDataAndTypeValidation()).required()
-        }).unknown().required()
-    };
+  const celebrateValidation = {
+    [Segments.BODY]: Joi.object({
+      customColumn: Joi.array().items(columnDataAndTypeValidation()).required(),
+    })
+      .unknown()
+      .required(),
+  };
 
-    return celebrate(celebrateValidation);
+  return celebrate(celebrateValidation);
 }
 
 async function parseColumnsType(req, res, next) {
-    if (!req.body.customColumns || !Array.isArray(req.body.customColumns)) {
-        throw new ValueError(400, "Invalid Custom Data Value");
-    }
+  if (!req.body.customColumns || !Array.isArray(req.body.customColumns)) {
+    throw new ValueError(400, "Invalid Custom Data Value");
+  }
 
-    const customColumns = await getType(req.body.customColumns);
+  const customColumns = await getType(req.body.customColumns);
 
-    if (!customColumns) {
-        throw new InternalError("Invalid Custom Columns");
-    }
+  if (!customColumns) {
+    throw new InternalError("Invalid Custom Columns");
+  }
 
-    req.body.customColumn = customColumns;
+  req.body.customColumn = customColumns;
 
-    next();
+  next();
 }
 
 function validateImportCustomColumns() {
-    const celebrateValidation = {
-        [Segments.BODY]: Joi.object({
-            importListID: Joi.number().min(1).required()
-        }).required()
-    };
+  const celebrateValidation = {
+    [Segments.BODY]: Joi.object({
+      importListID: Joi.number().min(1).required(),
+    }).required(),
+  };
 
-    return celebrate(celebrateValidation);
+  return celebrate(celebrateValidation);
 }
 
 module.exports = {
-    parseColumnsType: wrapAsync(parseColumnsType),
-    columnDataAndTypeValidation,
-    validate,
-    validateImportCustomColumns,
-    _: {
-        columnDataValidation: columnDataAndTypeValidation
-    }
+  parseColumnsType: wrapAsync(parseColumnsType),
+  columnDataAndTypeValidation,
+  validate,
+  validateImportCustomColumns,
+  _: {
+    columnDataValidation: columnDataAndTypeValidation,
+  },
 };
--- utils/validation/htmx/items.js
+++ utils/validation/htmx/items.js
@@ -3,212 +3,237 @@ const Joi = require("../extendedJoi");
 const { Item } = require("../../../models/items");
 const { User } = require("../../../models/users");
 const { CustomRowsItems } = require("../../../models/customUserData");
-const { EXTENDED_MAX_NUMBER_OF_ITEMS, MAX_NUMBER_OF_ITEMS } = require("./maximumLimitsOfElements");
+const {
+  EXTENDED_MAX_NUMBER_OF_ITEMS,
+  MAX_NUMBER_OF_ITEMS,
+} = require("./maximumLimitsOfElements");
 
-const textValidation = Joi.string().sanitize().trim().min(3).max(300).required();
-const uriValidation = Joi.alternatives().try(
-    Joi.string().sanitize().trim().max(10000).uri({
-        scheme: [
-            "http",
-            "https",
-        ],
-    }).required(),
-    Joi.string().trim().max(0).min(0).required(),
-).required();
+const textValidation = Joi.string()
+  .sanitize()
+  .trim()
+  .min(3)
+  .max(300)
+  .required();
+const uriValidation = Joi.alternatives()
+  .try(
+    Joi.string()
+      .sanitize()
+      .trim()
+      .max(10000)
+      .uri({
+        scheme: ["http", "https"],
+      })
+      .required(),
+    Joi.string().trim().max(0).min(0).required()
+  )
+  .required();
 
 function validateText(name, value) {
-    const schema = Joi.object({
-        [name]: textValidation,
-    }).required();
-    const { error, value: validatedValue } = schema.validate({
-        [name]: value,
-    });
-    return [error, validatedValue];
+  const schema = Joi.object({
+    [name]: textValidation,
+  }).required();
+  const { error, value: validatedValue } = schema.validate({
+    [name]: value,
+  });
+  return [error, validatedValue];
 }
 
 function validateURL(name, value) {
-    const schema = Joi.object({
-        [name]: uriValidation,
-    }).required();
-    const { error, value: validatedValue } = schema.validate({
-        [name]: value,
-    });
-    return [error, validatedValue];
+  const schema = Joi.object({
+    [name]: uriValidation,
+  }).required();
+  const { error, value: validatedValue } = schema.validate({
+    [name]: value,
+  });
+  return [error, validatedValue];
 }
 
 function validateStar(name, value) {
-    const schema = Joi.object({
-        [name]: Joi.number().min(0).max(5).required(),
-    });
-    const { error, value: validatedValue } = schema.validate({
-        [name]: value,
-    });
-    return [error, validatedValue];
+  const schema = Joi.object({
+    [name]: Joi.number().min(0).max(5).required(),
+  });
+  const { error, value: validatedValue } = schema.validate({
+    [name]: value,
+  });
+  return [error, validatedValue];
 }
 
 function validateCustomColumn(customColumn) {
-    const schema = columnDataAndTypeValidation();
-    const { error, value: validatedCustomColumn } = schema.validate(customColumn);
-    return [error, validatedCustomColumn];
+  const schema = columnDataAndTypeValidation();
+  const { error, value: validatedCustomColumn } = schema.validate(customColumn);
+  return [error, validatedCustomColumn];
 }
 
 function validateItemHeader(name, url, rating, errorMessages) {
-    var [error, validatedName] = validateText("Name", name);
-    if (error) {
-        errorMessages.name = error;
-    }
+  var [error, validatedName] = validateText("Name", name);
+  if (error) {
+    errorMessages.name = error;
+  }
 
-    var [error, validatedUrl] = validateURL("URL", url);
-    if (error) {
-        errorMessages.url = error;
-    }
+  var [error, validatedUrl] = validateURL("URL", url);
+  if (error) {
+    errorMessages.url = error;
+  }
 
-    var [error, validatedStar] = validateStar("Rating", rating);
-    if (error) {
-        errorMessages.rating = error;
-    }
+  var [error, validatedStar] = validateStar("Rating", rating);
+  if (error) {
+    errorMessages.rating = error;
+  }
 
-    return [
-        validatedName,
-        validatedUrl,
-        validatedStar,
-    ];
+  return [validatedName, validatedUrl, validatedStar];
 }
 
 function validateCustomColumns(customColumns, errorMessages) {
-    const validatedCustomColumns = [];
-    for (let customColumn of customColumns) {
-        const [error, validatedCustomColumn] = 
-            validateCustomColumn(customColumn);
+  const validatedCustomColumns = [];
+  for (let customColumn of customColumns) {
+    const [error, validatedCustomColumn] = validateCustomColumn(customColumn);
 
-        if (error) {
-            errorMessages[
-                customColumn.ListColumnTypeID || 
-                customColumn.baseListColumnTypeID ||
-                customColumn.CustomRowItemsID
-            ] = error;
-        }
-        validatedCustomColumns.push(validatedCustomColumn);
+    if (error) {
+      errorMessages[
+        customColumn.ListColumnTypeID ||
+          customColumn.baseListColumnTypeID ||
+          customColumn.CustomRowItemsID
+      ] = error;
     }
-    return validatedCustomColumns;
+    validatedCustomColumns.push(validatedCustomColumn);
+  }
+  return validatedCustomColumns;
 }
 
-async function isItemDuplicate(name, list, connection, id=null) {
-    const foundItem = await Item.findFromName(name, list, connection);
+async function isItemDuplicate(name, list, connection, id = null) {
+  const foundItem = await Item.findFromName(name, list, connection);
 
-    if (foundItem instanceof Item) {
-        if (typeof id === "bigint" && foundItem.id === id) {
-            return false;
-        }
-        return true;
+  if (foundItem instanceof Item) {
+    if (typeof id === "bigint" && foundItem.id === id) {
+      return false;
     }
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 async function checkIfUserCanCreateAnItem(userID, connection) {
-    const isUserBypassingRestriction = await User.isBypassingRestriction(userID, connection);
-    const numberOfItems = await Item.getCountFromUser(userID, connection);
+  const isUserBypassingRestriction = await User.isBypassingRestriction(
+    userID,
+    connection
+  );
+  const numberOfItems = await Item.getCountFromUser(userID, connection);
 
-    const maxNumberOfItems = isUserBypassingRestriction ?
-        EXTENDED_MAX_NUMBER_OF_ITEMS :
-        MAX_NUMBER_OF_ITEMS;
+  const maxNumberOfItems = isUserBypassingRestriction
+    ? EXTENDED_MAX_NUMBER_OF_ITEMS
+    : MAX_NUMBER_OF_ITEMS;
 
-    if (numberOfItems >= maxNumberOfItems) {
-        return `You cannot create more than ${maxNumberOfItems} items`;
-    } else {
-        return undefined;
-    }
+  if (numberOfItems >= maxNumberOfItems) {
+    return `You cannot create more than ${maxNumberOfItems} items`;
+  } else {
+    return undefined;
+  }
 }
 
-async function saveItem(name, url, rating, customColumns, parentList, connection) {
-    const newItem = new Item(name, url, parentList);
-    newItem.rating = rating;
+async function saveItem(
+  name,
+  url,
+  rating,
+  customColumns,
+  parentList,
+  connection
+) {
+  const newItem = new Item(name, url, parentList);
+  newItem.rating = rating;
 
-    if (!newItem.isValid()) {
-        return "Invalid list";
+  if (!newItem.isValid()) {
+    return "Invalid list";
+  }
+
+  await newItem.save(connection);
+
+  for (const customColumn of customColumns) {
+    const customUserData = new CustomRowsItems(
+      customColumn.Value,
+      newItem.id,
+      customColumn.ListColumnTypeID
+    );
+
+    if (customUserData.isValid()) {
+      await customUserData.save(connection);
     }
+  }
 
-    await newItem.save(connection);
-
-    for (const customColumn of customColumns) {
-        const customUserData = new CustomRowsItems(
-            customColumn.Value,
-            newItem.id,
-            customColumn.ListColumnTypeID
-        );
-
-        if (customUserData.isValid()) {
-            await customUserData.save(connection);
-        }
-    }
-
-    return null;
+  return null;
 }
 
 async function updateItem(
-    name, 
-    url, 
-    rating, 
-    customColumns, 
-    listColumnsType, 
-    parentList, 
-    item, 
-    connection
+  name,
+  url,
+  rating,
+  customColumns,
+  listColumnsType,
+  parentList,
+  item,
+  connection
 ) {
-    const updatedItem = item;
-    updatedItem.name = name;
-    updatedItem.url = url;
-    updatedItem.rating = rating;
+  const updatedItem = item;
+  updatedItem.name = name;
+  updatedItem.url = url;
+  updatedItem.rating = rating;
 
-    if (!updatedItem.isValid()) {
-        return "Invalid item";
+  if (!updatedItem.isValid()) {
+    return "Invalid item";
+  }
+
+  await updatedItem.save(connection);
+
+  const customData = [];
+  for (const columnType of listColumnsType) {
+    const foundCustomRow = item.customData.find(
+      (row) => row.columnTypeID === columnType.id
+    );
+    if (foundCustomRow) {
+      customData.push(foundCustomRow);
+    }
+  }
+
+  for (const customColumn of customColumns) {
+    let foundCustomData;
+    if (customColumn.CustomRowItemsID > 0) {
+      foundCustomData = customData.find(
+        (data) => data.id == customColumn.CustomRowItemsID
+      );
+    } else {
+      foundCustomData = new CustomRowsItems(
+        undefined,
+        item.id,
+        -customColumn.CustomRowItemsID
+      );
     }
 
-    await updatedItem.save(connection);
-
-    const customData = [];
-    for (const columnType of listColumnsType) {
-        const foundCustomRow = item.customData.find(row => row.columnTypeID === columnType.id);
-        if (foundCustomRow) {
-            customData.push(foundCustomRow);
-        }
+    if (!foundCustomData) {
+      continue;
     }
 
-    for (const customColumn of customColumns) {
-        let foundCustomData;
-        if (customColumn.CustomRowItemsID > 0) {
-            foundCustomData = customData.find(data => data.id == customColumn.CustomRowItemsID);
-        } else {
-            foundCustomData = new CustomRowsItems(undefined, item.id, -customColumn.CustomRowItemsID);
-        }
+    foundCustomData.value = customColumn.Value.toString();
 
-        if (!foundCustomData) {
-            continue;
-        }
-
-        foundCustomData.value = customColumn.Value.toString();
-
-        if (foundCustomData.isValid()) {
-            await foundCustomData.save(connection);
-        } else {
-            await foundCustomData.delete(connection);
-        }
+    if (foundCustomData.isValid()) {
+      await foundCustomData.save(connection);
+    } else {
+      await foundCustomData.delete(connection);
     }
+  }
 
-    return null;
+  return null;
 }
 
 module.exports = {
-    validateText,
-    validateURL,
-    validateStar,
-    validateItemHeader,
-    validateCustomColumns,
-    isItemDuplicate,
-    checkIfUserCanCreateAnItem,
-    saveItem,
-    updateItem,
-    textValidation,
-    uriValidation,
+  validateText,
+  validateURL,
+  validateStar,
+  validateItemHeader,
+  validateCustomColumns,
+  isItemDuplicate,
+  checkIfUserCanCreateAnItem,
+  saveItem,
+  updateItem,
+  textValidation,
+  uriValidation,
 };
