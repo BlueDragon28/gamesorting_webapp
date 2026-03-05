ab751d32ee3db26057599c6bad47c4579bb9de89
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2026-03-05 21:27:58 +0100

    handling moving back from item to the list of items using js

--- public/javascript/htmx/collections_handling.js
+++ public/javascript/htmx/collections_handling.js
@@ -4,7 +4,9 @@ const BUTTON_LIST_SRC_STR = "button-collection-list";
 const BUTTON_LIST_ID_STARTSWITH = "button-list-";
 const BUTTON_LIST_COLLECTION_CURRENT_PAGE_STARTSWITH =
   "button-collection-current-page-";
+const BUTTON_GO_BACK_TO_LIST = "button-go-back-to-list";
 const LIST_ITEMS_BLOCK_ID = "#collections-items-list-row";
+const ITEM_BLOCK_ID = "#item-detail-card";
 
 const BUTTON_ITEM_FROM_LIST_STR = "button-item-from-list";
 const BUTTON_ITEM_FROM_LIST_STARTSWITH = "item-from-list-id-";
@@ -53,6 +55,7 @@ function load_collection(targetCollectionID, page_id) {
   console.log("target id:", id);
   console.log("ready to replace");
   let headers = {};
+  console.log("page_id:", page_id);
   if (page_id) {
     headers["GS-currentPage"] = `page_id`;
     headers["GS-currentItemsPage"] = listPageID;
@@ -61,6 +64,20 @@ function load_collection(targetCollectionID, page_id) {
     target: "#collections-lists-global-row",
     swap: "outerHTML",
     push: "true",
+    headers,
+  });
+}
+
+function load_list(listID, blockID = LIST_ITEMS_BLOCK_ID) {
+  const listPageID = getListPage(listID);
+  let headers = {
+    "GS-currentItemsPage": listPageID,
+  };
+
+  htmx.ajax("GET", `/collections/lists/${listID}?onlyItems=true`, {
+    target: blockID,
+    swap: "outerHTML",
+    push: true,
     headers,
   });
 }
@@ -182,12 +199,24 @@ function handling_open_item(element) {
   return true;
 }
 
+function handling_go_back_to_list(element) {
+  if (!element.classList.contains(BUTTON_GO_BACK_TO_LIST)) return false;
+
+  const list_id = element.getAttribute(GS_CURRENT_ID);
+  if (typeof list_id !== "string" || list_id.length === 0) return false;
+
+  load_list(list_id, ITEM_BLOCK_ID);
+
+  return true;
+}
+
 document.body.addEventListener("click", function (event) {
   if (event.target) {
     handling_open_list(event.target) ||
       handling_open_item(event.target) ||
       handling_next_list(event.target) ||
-      handling_previous_list(event.target);
+      handling_previous_list(event.target) ||
+      handling_go_back_to_list(event.target);
   }
 
   console.log("test");
