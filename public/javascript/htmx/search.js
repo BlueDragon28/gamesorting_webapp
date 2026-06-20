3141f370b20972abdfca33af54bb8b67195352ba
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2026-06-20 16:33:49 +0200

    reformat search.js file

--- public/javascript/htmx/search.js
+++ public/javascript/htmx/search.js
@@ -1,61 +1,60 @@
 function getListIDFromLocation() {
-    const currentLocation = window.location.pathname;
-    const valueRegex = /^\/collections\/lists\/([0-9]+)$/
-    if (!valueRegex.test(currentLocation)) {
-        throw new Error("Cannot make a search request");
-    }
+  const currentLocation = window.location.pathname;
+  const valueRegex = /^\/collections\/lists\/([0-9]+)$/;
+  if (!valueRegex.test(currentLocation)) {
+    throw new Error("Cannot make a search request");
+  }
 
-    const listID = parseInt(currentLocation.match(valueRegex)[1]);
-    if (listID === undefined || isNaN(listID)) {
-        throw new Error("Failed to get listID number");
-    }
+  const listID = parseInt(currentLocation.match(valueRegex)[1]);
+  if (listID === undefined || isNaN(listID)) {
+    throw new Error("Failed to get listID number");
+  }
 
-    return listID;
+  return listID;
 }
 
 function submitEvent(searchInput) {
-    if (!searchInput) {
-        return console.error("Invalid search input");
-    }
+  if (!searchInput) {
+    return console.error("Invalid search input");
+  }
 
-    const searchTerm = searchInput.value;
+  const searchTerm = searchInput.value;
 
-    const collectionsItemsList = document.getElementById("collections-items-list-row");
-    if (!collectionsItemsList) {
-        return console.error("collection items list not available!");
-    }
+  const collectionsItemsList = document.getElementById(
+    "collections-items-list-row",
+  );
+  if (!collectionsItemsList) {
+    return console.error("collection items list not available!");
+  }
 
-    let listID;
-    try {
-        listID = getListIDFromLocation();
-    } catch (e) {
-        return console.log(e.message);
-    }
+  let listID;
+  try {
+    listID = getListIDFromLocation();
+  } catch (e) {
+    return console.log(e.message);
+  }
 
-    htmx.ajax(
-        "GET",
-        `/collections/lists/${listID}?onlyItems=true`,
-        {
-            target: collectionsItemsList,
-            swap:"outerHTML",
-            headers: {
-                "GS-searchTerm": searchTerm,
-            },
-        },
-    )
-        .then(() => {})
-        .catch((_) => {
-            console.error("Oups: something went wrong!");
-        });
+  htmx
+    .ajax("GET", `/collections/lists/${listID}?onlyItems=true`, {
+      target: collectionsItemsList,
+      swap: "outerHTML",
+      headers: {
+        "GS-searchTerm": searchTerm,
+      },
+    })
+    .then(() => {})
+    .catch((_) => {
+      console.error("Oups: something went wrong!");
+    });
 }
 
-document.body.addEventListener("submit", function(event) {
-    if (event.target.id !== "search-box") {
-        return;
-    }
+document.body.addEventListener("submit", function (event) {
+  if (event.target.id !== "search-box") {
+    return;
+  }
 
-    event.preventDefault();
-    
-    const searchInput = event.target.firstElementChild;
-    submitEvent(searchInput);
+  event.preventDefault();
+
+  const searchInput = event.target.firstElementChild;
+  submitEvent(searchInput);
 });
